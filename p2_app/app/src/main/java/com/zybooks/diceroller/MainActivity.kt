1f14fedb3f32875bb8b9a0c961ee76fdf271a1ae
okquentin <qosterhage@outlook.com>
2025-02-20 23:24:58 -0500

    P2 Basic Functionality Working
    As provided by Zybooks

---
+++ p2_app/app/src/main/java/com/zybooks/diceroller/MainActivity.kt
@@ -0,0 +1,229 @@
+package com.zybooks.diceroller
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.ContextMenu
+import android.view.GestureDetector
+import android.view.Menu
+import android.view.MenuItem
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.GestureDetectorCompat
+import kotlin.math.abs
+
+const val MAX_DICE = 3
+
+class MainActivity : AppCompatActivity(),
+    RollLengthDialogFragment.OnRollLengthSelectedListener{
+
+    private var numVisibleDice = MAX_DICE
+    private lateinit var diceList: MutableList<Dice>
+    private lateinit var diceImageViewList: MutableList<ImageView>
+    private lateinit var optionsMenu: Menu
+    private var timer: CountDownTimer? = null
+    private var selectedDie = 0
+    private var timerLength = 2000L
+    private var initTouchX = 0
+    private lateinit var gestureDetector: GestureDetectorCompat
+
+
+    override fun onRollLengthClick(which: Int) {
+        // Convert to milliseconds
+        timerLength = 1000L * (which + 1)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Create list of Dice
+        diceList = mutableListOf()
+        for (i in 0 until MAX_DICE) {
+            diceList.add(Dice(i + 1))
+        }
+
+        // Create list of ImageViews
+        diceImageViewList = mutableListOf(
+            findViewById(R.id.dice1), findViewById(R.id.dice2), findViewById(R.id.dice3))
+
+        showDice()
+
+        // Register context menus for all dice and tag each die
+        for (i in 0 until diceImageViewList.size) {
+            registerForContextMenu(diceImageViewList[i])
+            diceImageViewList[i].tag = i
+        }
+
+        // Moving finger left or right changes dice number
+        diceImageViewList[0].setOnTouchListener { v, event ->
+            var returnVal = true
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    initTouchX = event.x.toInt()
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    val x = event.x.toInt()
+
+                    // See if movement is at least 20 pixels
+                    if (abs(x - initTouchX) >= 20) {
+                        if (x > initTouchX) {
+                            diceList[0].number++
+                        } else {
+                            diceList[0].number--
+                        }
+                        showDice()
+                        initTouchX = x
+                    }
+                }
+                MotionEvent.ACTION_UP -> {
+                    v.performClick() // Ensures proper click handling
+                }
+                else -> returnVal = false
+            }
+            returnVal
+        }
+
+        gestureDetector = GestureDetectorCompat(this,
+            object : GestureDetector.SimpleOnGestureListener() {
+                override fun onDown(e: MotionEvent): Boolean {
+                    return true
+                }
+
+                override fun onFling(
+                    e1: MotionEvent?,
+                    e2: MotionEvent,
+                    velocityX: Float,
+                    velocityY: Float
+                ): Boolean {
+                    rollDice()
+                    return true
+                }
+            }
+        )
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        gestureDetector.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        // Determine which menu option was chosen
+        return when (item.itemId) {
+            R.id.action_one -> {
+                changeDiceVisibility(1)
+                showDice()
+                true
+            }
+            R.id.action_two -> {
+                changeDiceVisibility(2)
+                showDice()
+                true
+            }
+            R.id.action_three -> {
+                changeDiceVisibility(3)
+                showDice()
+                true
+            }
+            R.id.action_stop -> {
+                timer?.cancel()
+                item.isVisible = false
+                true
+            }
+            R.id.action_roll -> {
+                rollDice()
+                true
+            }
+            R.id.action_roll_length -> {
+                val dialog = RollLengthDialogFragment()
+                dialog.show(supportFragmentManager, "rollLengthDialog")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.appbar_menu, menu)
+        optionsMenu = menu!!
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        // Save which die is selected
+        selectedDie = v?.tag as Int
+
+        menuInflater.inflate(R.menu.context_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.add_one -> {
+                diceList[selectedDie].number++
+                showDice()
+                true
+            }
+            R.id.subtract_one -> {
+                diceList[selectedDie].number--
+                showDice()
+                true
+            }
+            R.id.roll -> {
+                rollDice()
+                true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+    private fun showDice() {
+
+        // Show visible dice
+        for (i in 0 until numVisibleDice) {
+            val diceDrawable = ContextCompat.getDrawable(this, diceList[i].imageId)
+            diceImageViewList[i].setImageDrawable(diceDrawable)
+            diceImageViewList[i].contentDescription = diceList[i].imageId.toString()
+        }
+    }
+
+    private fun changeDiceVisibility(numVisible: Int) {
+        numVisibleDice = numVisible
+
+        // Make dice visible
+        for (i in 0 until numVisible) {
+            diceImageViewList[i].visibility = View.VISIBLE
+        }
+
+        // Hide remaining dice
+        for (i in numVisible until MAX_DICE) {
+            diceImageViewList[i].visibility = View.GONE
+        }
+    }
+
+    private fun rollDice() {
+        optionsMenu.findItem(R.id.action_stop).isVisible = true
+        timer?.cancel()
+
+        // Start a timer that periodically changes each visible dice
+        timer = object : CountDownTimer(timerLength, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                for (i in 0 until numVisibleDice) {
+                    diceList[i].roll()
+                }
+                showDice()
+            }
+
+            override fun onFinish() {
+                optionsMenu.findItem(R.id.action_stop).isVisible = false
+            }
+        }.start()
+    }
+}
+
